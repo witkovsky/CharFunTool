caef33b5125c3141de914048ee227ebc1bdbb6c3
Viktor <witkovsky@gmail.com>
2017-05-10 18:38:55 +0200

    cf4Gamma and cf4ChiSquared

---
+++ CharacteristicFunctions/cf4ChiSquared.m
@@ -0,0 +1,151 @@
+function cf = cf4ChiSquared(t,df,ncp,coef,n)
+%CF4CHISQUARED Characteristic function of the distribution of a convolution
+% (or a linear combination) of independent CHI-SQUARED random variables X_i
+% ~ CHI2(df,ncp), where X_i ~ CHI2(df(i),ncp(i)), and df(i) and  ncp(i) represent the 'degrees of freedom' and
+%  the 'non-centrality' parameters of the CHI-SQUARED distribution.   
+%
+%  In particular, cf4ChiSquared(t,df,ncp,coef) evaluates the characteristic
+%  function cf(t) of Y = coef(1) * X_1 + ... + coef(N) * X_N, where X_i ~
+%  CHI2(df(i),ncp(i)), and k(i) and  theta(i) represent the 'shape' and
+%  the 'scale' parameters of the GAMMA distribution.  
+%
+%  The characteristic function of Y is defined by
+%   cf(t) = Prod ( (1-2*i*t*coef(i))^(-df(i)/2)
+%                   * exp((i*t*ncp(i))/(1-2*i*t*coef(i))) )
+%
+%  For more details see, e.g., WIKIPEDIA:
+%  https://en.wikipedia.org/wiki/Chi-squared_distribution
+%  https://en.wikipedia.org/wiki/Noncentral_chi-squared_distribution
+%
+% SYNTAX
+%  cf = cf4ChiSquared(t,df,ncp,coef,n)
+% 
+% INPUTS:
+%  t     - vector or array of real values, where the CF is evaluated.
+%  df    - vector of the degrees of freedom of the the chi-squared random
+%          variables.  If df is scalar, it is assumed that all degrees of
+%          freedom are equal. If empty, default value is df = 1.
+%  ncp   - vector of the non-centrality parameters of the the chi-squared
+%          random variables. If ncp is scalar, it is assumed that all
+%          non-centrality parameters are equal. If empty, default value is
+%          ncp = 0. 
+%  coef  - vector of the coefficients of the lienar combination of the
+%          chi-squared random variables. If coef is scalar, it is assumed
+%          that all coefficients are equal. If empty, default value is
+%          coef = 1.
+%  n     - scalar convolution coeficient n, such that Z = Y + ... + Y is
+%          sum of n iid random variables Y, where each Y = sum_{i=1}^N
+%          coef(i) * CHI2(df(i),ncp(i))) is independently and identically
+%          distributed random variable. If empty, default value is n = 1.   
+%
+% EXAMPLE 1:
+% % CF of the distribution of chi-squared RV with DF = 1, NCP = 1
+%   df   = 1;
+%   ncp  = 1;
+%   coef = 1;
+%   t    = linspace(-100,100,501);
+%   cf   = cf4ChiSquared(t,df,ncp,coef);
+%   figure; plot(t,real(cf),t,imag(cf));grid on
+%   title('Characteristic function of the \chi^2 RV with DF=1 and NCP=1')
+%
+% EXAMPLE 2: 
+% % CF of a linear combination of K=100 independent chi2_1 RVs
+%   K    = 100;
+%   idx  = 1:K;
+%   coef = 1./((idx - 0.5)*pi).^2;
+%   figure; plot(idx,coef,'.-')
+%   title('Coefficients of the linear combination of \chi^2 RVs with DF=1')
+%   df   = 1;
+%   cf   = cf4ChiSquared(t,df,[],coef);
+%   t    = linspace(-100,100,501);
+%   figure; plot(t,real(cf),t,imag(cf));grid on
+%   title('Characteristic function of the linear combination of \chi^2_1 RVs')
+%
+% EXAMPLE 3:
+% % PDF/CDF from the CF by cf2DistGP
+%   df   = 1;
+%   coef = 1./(((1:100) - 0.5)*pi).^2;
+%   cf = @(t) cf4ChiSquared(t,df,[],coef);
+%   clear options
+%   options.N = 2^12;
+%   options.xMin = 0;
+%   result = cf2DistGP(cf,[],[],options);
+%
+% REFERENCES
+%  IMHOF J. (1961): Computing the distribution of quadratic forms in normal
+%  variables. Biometrika 48, 419–426.
+
+% (c) 2017 Viktor Witkovsky (witkovsky@gmail.com)
+% Ver.: 10-May-2017 18:11:50
+
+%% Check and set the default input parameters
+narginchk(1, 5);
+if nargin < 5, n = []; end
+if nargin < 4, coef = []; end
+if nargin < 3, ncp  = []; end
+if nargin < 2, df   = []; end
+
+if isempty(ncp) && ~isempty(df)
+    isNoncentral = false; ncp = 0;
+elseif isempty(ncp) && ~isempty(coef)
+    isNoncentral = false; ncp = 0;
+elseif ~any(ncp)
+    isNoncentral = false; ncp = 0;
+else
+    isNoncentral = true;
+end
+
+if isempty(df) && ~isempty(coef)
+    df = 1;
+elseif isempty(df) && ~isempty(ncp)
+    df = 1;
+end
+
+if isempty(coef) && ~isempty(ncp)
+    coef = 1;
+elseif isempty(coef) && ~isempty(df)
+    coef = 1;
+end
+
+if isempty(n)
+    n = 1;
+end
+%% Find the unique coefficients and their multiplicities
+if ~isempty(coef) && isscalar(df) && ~isNoncentral 
+    coef = sort(coef);
+    m    = length(coef);
+    [coef,idx] = unique(coef);
+    df = df * diff([idx;m+1]);
+end
+
+% Check/set equal dimensions for the vectors coef, df, and ncp
+[errorcode,coef,df,ncp] = distchck(3,coef(:)',df(:)',ncp(:)');
+if errorcode > 0
+    error(message('InputSizeMismatch'));
+end
+
+%% ALGORITHM 
+% Characteristic function of a linear combination of noncentral chi-squares
+
+szt   = size(t);
+t     = t(:);
+cf    = 1;
+aux   = bsxfun(@times,t,coef);
+if isNoncentral
+    aux = bsxfun(@power,(1 -2i * aux),-df/2) .* ...
+        exp(bsxfun(@times,aux,1i*ncp)./(1-2i*aux));
+else
+    aux = bsxfun(@power,(1 -2i * aux),-df/2);
+end
+cf   = cf .* prod(aux,2);
+cf   = reshape(cf,szt);
+
+if ~isempty(n)
+    if isscalar(n)
+        cf = cf .^ n;
+    else
+        error('n should be a scalar (positive integer) value');
+    end
+end
+
+end
