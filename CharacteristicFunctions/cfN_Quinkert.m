116cd67198ff7266656a122249b3296589ee9cd4
Viktor <witkovsky@gmail.com>
2017-05-14 16:43:17 +0200

    Rename cf4 to cf_

--- CharacteristicFunctions/cfN_Quinkert.m
+++ CharacteristicFunctions/cfN_Quinkert.m
@@ -85,49 +85,39 @@ cf = reshape(cf,szt);
 cf(t==0) = 1;
 
 end
-%% HYPERGEOM1F1
+%% Function hypergeom1F1
 function f = hypergeom1F1(a,b,z)
-%HYPERGEOM1F1 Computes the confluent hypergeometric function 1F1(a,b,z), 
-% also known as the Kummer function M(a,b,z), for the real parameters a 
-% and b (here assumed to be scalars), and the complex argument z 
+%HYPERGEOM1F1 Computes the confluent hypergeometric function 1F1(a,b,z),
+% also known as the Kummer function M(a,b,z), for the real parameters a
+% and b (here assumed to be scalars), and the complex argument z
 % (could be scalar, vector or array).
 %
 % The algorithm is based on a Fortran program in S. Zhang & J. Jin
 % "Computation of Special Functions" (Wiley, 1996).
 % http://iris-lee3.ece.uiuc.edu/~jjin/routines/routines.html
 %
-% Converted by using f2matlab: https://sourceforge.net/projects/f2matlab/  
+% Converted by using f2matlab: https://sourceforge.net/projects/f2matlab/
 % written by Ben Barrowes (barrowes@alum.mit.edu)
 %
 % SYNTAX
 %   f = hypergeom1F1(a,b,z)
 %
 % EXAMPLE1 (CF of Beta(1/2,1/2) distribution)
-% a = 1/2;
-% b = 1;
-% t = linspace(-50,50,2^11)';
-% z = 1i*t;
-% cf =  hypergeom1F1(a,b,z);
+% a  = 1/2;
+% b  = 1/2;
+% t  = linspace(-50,50,2^11)';
+% cf =  hypergeom1F1(a,b+b,1i*t);
 % figure; plot(t,real(cf),t,imag(cf))
 % title('Characteristic function of Beta(1/2,1/2) distribution')
 % xlabel('t')
 % ylabel('CF')
 %
-% See also: hypergeom1F1, hypergeom2F1, hypergeomU, kummerM, kummerU, 
-%        whittakerM, whittakerW, gammaincc,
-%
 % NOTE1: the functions hypergeom1F1 and kummerM are equivalent
-%
-% NOTE2: the functions based on hypergeomU resp. kummerU are badly
-%        conditioned for when b is negative integer. In such situations,
-%        approximate the the function value by using the noninteger 
-%        parameter b, say b = b + eps, for some small eps.
 
 % Viktor Witkovsky (witkovsky@gmail.com)
 % Ver.: 07-Aug-2015 10:19:29
 
 %% CHECK THE PARAMETERS
-
 sz = size(z);
 
 if max(sz) > 1
@@ -148,11 +138,11 @@ function [chg]=cchg(a,b,z)
 %     Purpose: Compute confluent hypergeometric function
 %     M(a,b,z) with real parameters a, b and a
 %     complex argument z
-%     Input :  
+%     Input :
 %     a --- Parameter
 %     b --- Parameter
 %     z --- Complex argument
-%     Output:  
+%     Output:
 %     CHG --- M(a,b,z)
 %     Routine called: GAMMA for computing gamma function
 %     ===================================================
@@ -165,17 +155,17 @@ a0  = a;
 
 if(b == 0 || b == -fix(abs(b)))
     chg = 1e+300;
-elseif(a == 0 || z == 0);
+elseif(a == 0 || z == 0)
     chg = 1;
-elseif(a == -1),
+elseif(a == -1)
     chg = 1 - z./b;
-elseif (a == b), 
+elseif (a == b)
     chg = exp(z);
-elseif(a-b == 1),
+elseif(a-b == 1)
     chg = (1 + z / b) * exp(z);
-elseif(a == 1 && b == 2),
+elseif(a == 1 && b == 2)
     chg = (exp(z) - 1) / z;
-elseif(a == fix(a) && a < 0);
+elseif(a == fix(a) && a < 0)
     m   = fix(-a);
     cr  = 1;
     chg = 1;
@@ -198,14 +188,14 @@ else
         la = fix(a);
         a  = a - la - 1;
     end
-    for  n = 0:nl;
+    for  n = 0:nl
         if(a0 >= 2)
-            a = a + 1; 
+            a = a + 1;
         end
         if (abs(z)< 20 + abs(b) || a < 0)
             chg = 1;
             crg = 1;
-            for  j = 1:500,
+            for  j = 1:500
                 crg = crg * (a+j-1) / (j * (b + j - 1)) * z;
                 chg = chg + crg;
                 if(abs((chg - chw) / chg) < 1e-15)
@@ -222,7 +212,7 @@ else
             cs2 = 1;
             cr1 = 1;
             cr2 = 1;
-            for  i = 1:8,
+            for  i = 1:8
                 cr1 = -cr1 * (a + i - 1) * (a - b + i) / (z * i);
                 cr2 =  cr2 * (b - a + i - 1) * (i - a) / (z * i);
                 cs1 =  cs1 + cr1;
@@ -259,7 +249,7 @@ else
         end
     end
     if(a0 >= 2)
-        for  i = 1:la-1,
+        for  i = 1:la-1
             chg = ((2 * a - b + z) * cy1 + (b-a) * cy0) / a;
             cy0 = cy1;
             cy1 = chg;
@@ -282,11 +272,11 @@ function ga=gamma(x)
 %     ==================================================
 g  = zeros(1,26);
 pi = 3.141592653589793;
-if(x == fix(x));
+if(x == fix(x))
     if (x > 0)
         ga = 1;
         m1 = x - 1;
-        for  k = 2:m1,
+        for  k = 2:m1
             ga = ga * k;
         end
     else
@@ -297,7 +287,7 @@ else
         z = abs(x);
         m = fix(z);
         r = 1;
-        for  k = 1:m,
+        for  k = 1:m
             r = r .* (z - k);
         end
         z = z - m;
@@ -312,14 +302,14 @@ else
         .50020075d-8,-.11812746d-8,.1043427d-9,.77823d-11,-.36968d-11,...
         .51d-12,-.206d-13,-.54d-14,.14d-14,.1d-15];
     gr=g(26);
-    for k = 25:-1:1,
+    for k = 25:-1:1
         gr = gr * z + g(k);
     end
     ga = 1 / (gr * z);
     if(abs(x)> 1)
         ga = ga * r;
         if(x < 0)
-            ga = -pi / (x * ga * sin(pi * x)); 
+            ga = -pi / (x * ga * sin(pi * x));
         end
     end
 end
--- CharacteristicFunctions/cf_Gamma.m
+++ CharacteristicFunctions/cf_Gamma.m
@@ -1,19 +1,18 @@
-function cf = cf4InverseGamma(t,alpha,beta,coef,n)
-%%cf4InverseGamma Characteristic function of a linear combination (or
-%  convolution) of independent Inverse-Gamma random variables, X_i~IGamma. 
+function cf = cf_Gamma(t,alpha,beta,coef,n)
+%%cf_Gamma Characteristic function of a linear combination (resp.
+%  convolution) of independent GAMMA random variables, X ~
+%  Gamma(alpha,beta).  
 %
-%  In particular, cf4InverseGamma(t,alpha,beta,coef,n) evaluates the
-%  characteristic function cf(t) of Y = coef(1) * X_1 + ... + coef(N) *
-%  X_N, where X_i ~ IGamma(alpha(i),beta(i)), and alpha(i) and  beta(i)
-%  represent the 'shape' and the 'rate' parameters of the Inverse-Gamma
-%  distribution. 
+%  In particular, cf_Gamma(t,alpha,beta,coef) evaluates the characteristic
+%  function cf(t) of Y = coef(1) * X_1 + ... + coef(N) * X_N, where X_i ~
+%  GAMMA(alpha(i),beta(i)), and alpha(i) and  beta(i) represent the 'shape'
+%  and the 'rate' parameters of the GAMMA distribution.
 %
 %  The characteristic function of Y is defined by
-%   cf(t) = Prod( 2 / gamma(alpha(i)) * (-1i*beta(i)*t).^(alpha(i)/2) ...
-%                 * besselk(alpha(i),sqrt(-4i*beta(i)*t)) )
+%   cf(t) = Prod( (1 - i*t*coef(i)/beta(i))^(-alpha(i)) )
 %
 % SYNTAX:
-%  cf = cf4InverseGamma(t,alpha,beta,coef,n)
+%  cf = cf_Gamma(t,alpha,beta,coef,n)
 %
 % INPUTS:
 %  t     - vector or array of real values, where the CF is evaluated.
@@ -22,67 +21,61 @@ function cf = cf4InverseGamma(t,alpha,beta,coef,n)
 %  beta  - vector of the 'rate' parameters beta > 0. If empty, default
 %          value is beta = 1.  
 %  coef  - vector of the coefficients of the linear combination of the
-%          IGamma random variables. If coef is scalar, it is assumed
+%          GAMMA random variables. If coef is scalar, it is assumed
 %          that all coefficients are equal. If empty, default value is
 %          coef = 1.
 %  n     - scalar convolution coeficient n, such that Z = Y + ... + Y is
 %          sum of n iid random variables Y, where each Y = sum_{i=1}^N
-%          coef(i) * GAMMA(k(i),theta(i))) is independently and identically
-%          distributed random variable. If empty, default value is n = 1.   
-%
+%          coef(i) * X_i, with X_i ~ Gamma(alpha(i),beta(i))) independently
+%          and identically distributed random variables. If empty, default
+%          value is n = 1.    
+% 
 % PARAMETRIZATION:
-%   As for the GAMMA distribution, also for the Inverse-Gamma distribution
-%   there are three different possible parametrizations: 
+%   Notice that there are three different parametrizations for GAMMA
+%   distribution in common use: 
 %   i)   With a shape parameter k and a scale parameter theta.
 %   ii)  With a shape parameter alpha = k and an inverse scale parameter
 %        beta = 1/theta, called a rate parameter. 
 %   iii) With a shape parameter k and a mean parameter mu = k/beta.
 %   In each of these three forms, both parameters are positive real numbers.
 %
-%   Here, cf4InverseGamma implements the shape-rate parametrization with
+%   Here, cf_Gamma implements the shape-rate parametrization with
 %   parameters alpha and beta, respectively. 
 %
-%   If X ~ IGamma(df/2,1/2)(shape-rate parametrization), then X is identical
-%   to ICHI2(df), the Inverse-Chi-squared distribution with df degrees of
-%   freedom.
+%   If X ~ Gamma(df/2,1/2)(shape-rate parametrization), then X is identical
+%   to CHI2(df), the chi-squared distribution with df degrees of freedom.  
 %
 % WIKIPEDIA: 
-%   https://en.wikipedia.org/wiki/Inverse-gamma_distribution.
+%   https://en.wikipedia.org/wiki/Gamma_distribution.
 %
 % EXAMPLE 1:
-% % CF of a linear combination of K=100 independent IGamma RVs
-%   K    = 50;
-%   t    = linspace(-100,100,201);
-%   idx  = 1:K;
-%   coef = 1./((idx - 0.5)*pi).^2;
-%   figure; plot(idx,coef,'.-')
-%   title('Coefficients of the linear combination of IGamma RVs')
-%   alpha= 5/2;
-%   beta = 2;
-%   cf   = cf4InverseGamma(t,alpha,beta,coef);
-%   figure; plot(t,real(cf),t,imag(cf))
-%   title('Characteristic function of the linear combination of IGamma RVs')
+% % CF of a linear combination of K=100 independent Gamma RVs)
+%   coef = 1./(((1:50) - 0.5)*pi).^2;
+%   figure; plot(coef,'.-');grid on
+%   title('Coefficients of the linear combination of GAMMA RVs')
+%   alpha = 5/2;
+%   beta = 1/2;
+%   t = linspace(-10,10,201);
+%   cf = cf_Gamma(t,alpha,beta,coef);
+%   figure; plot(t,real(cf),t,imag(cf));grid on
+%   title('Characteristic function of the linear combination of GAMMA RVs')
 %
 % EXAMPLE 2:
 % % PDF/CDF from the CF by cf2DistGP
-%   alpha= 5/2;
+%   alpha = 5/2;
 %   beta = 1/2;
 %   coef = 1./(((1:50) - 0.5)*pi).^2;
-%   cf   = @(t) cf4InverseGamma(t,alpha,beta,coef);
+%   cf = @(t) cf_Gamma(t,alpha,beta,coef);
 %   clear options
-%   options.N    = 2^10;
+%   options.N = 2^10;
 %   options.xMin = 0;
 %   result = cf2DistGP(cf,[],[],options);
-%
-% REFERENCES
-%  WITKOVSKY, V. (2001). Computing the distribution of a linear combination
-%  of inverted gamma variables. Kybernetika 37, 79–90.
 
 % (c) 2017 Viktor Witkovsky (witkovsky@gmail.com)
 % Ver.: 10-May-2017 18:11:50
 
 %% ALGORITHM
-% cf = cf4InverseGamma(t,alpha,beta,coef,n)
+% cf = cf_Gamma(t,alpha,beta,coef,n)
 
 %% CHECK THE INPUT PARAMETERS
 narginchk(1, 5);
@@ -93,17 +86,17 @@ if nargin < 2, alpha = []; end
 
 %%
 if isempty(beta) && ~isempty(alpha)
-    beta = 2;
+    beta = 1;
 elseif isempty(beta) && ~isempty(coef)
-    beta = 2;
+    beta = 1;
 elseif ~any(beta)
-    beta = 2;
+    beta = 1;
 end
 
 if isempty(alpha) && ~isempty(coef)
-    alpha = 2;
+    alpha = 1;
 elseif isempty(alpha) && ~isempty(beta)
-    alpha = 2;
+    alpha = 1;
 end
 
 if isempty(coef) && ~isempty(beta)
@@ -112,7 +105,18 @@ elseif isempty(coef) && ~isempty(alpha)
     coef = 1;
 end
 
+if isempty(n)
+    n = 1;
+end
+
 %% Equal size of the parameters   
+if ~isempty(coef) && isscalar(alpha) && isscalar(beta) && isempty(n)
+    coef = sort(coef);
+    m    = length(coef);
+    [coef,idx] = unique(coef);
+    alpha = alpha * diff([idx;m+1]);
+end
+
 [errorcode,coef,alpha,beta] = distchck(3,coef(:)',alpha(:)',beta(:)');
 if errorcode > 0
     error(message('InputSizeMismatch'));
@@ -134,10 +138,8 @@ for j = 1:idc(end)
     idx1 = min(idc(j)*szcLimit,szcoefs);
     idx  = idx0:idx1;
     idx0 = idx1+1;
-    aux  = (bsxfun(@times,-1i*t,coef(idx)./beta(idx))).^(1/2);
-    aux  = 2*bsxfun(@power,aux,alpha(idx)) .* ...
-           besselk(ones(length(t),1)*alpha(idx),2*aux);
-    aux  = bsxfun(@times,aux,1./gamma(alpha(idx)));
+    aux  = bsxfun(@times,t,coef(idx)./beta(idx));
+    aux  = bsxfun(@power,(1 - 1i * aux),-alpha(idx));
     cf   = cf .* prod(aux,2);
 end
 cf = reshape(cf,szt);
