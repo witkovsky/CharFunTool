6f9011825d10fccb77e1360ad81bb97c716b0836
Viktor <witkovsky@gmail.com>
2017-06-05 13:58:35 +0200

    Log Inverse Gamma RV

--- CharacteristicFunctions/cf_LogInverseGammaRV.m
+++ CharacteristicFunctions/cf_LogInverseGammaRV.m
@@ -1,26 +1,31 @@
-function cf = cf_LogGammaRV(t,alpha,beta,coef,n)
-%%cf_LogGammaRV Characteristic function of a linear combination (resp.
-%  convolution) of independent log-transformed random variables (RVs)
-%  log(X), where X ~ Gamma(alpha,beta), and alpha and  beta represent the
-%  'shape' and the 'rate' parameters of the GAMMA distribution.
+function cf = cf_LogInverseGammaRV(t,alpha,beta,coef,n)
+%%cf_LogInverseGammaRV Characteristic function of a linear combination
+%  (resp. convolution) of independent log-transformed random variables
+%  (RVs) log(X), where X ~ InvGamma(alpha,beta), and alpha and  beta
+%  represent the 'shape' and the 'rate' parameters of the Inverse Gamma
+%  distribution. Notice that Y = log(X) = log(1/Z) where  X ~
+%  InvGamma(alpha,beta) and  Z ~ Gamma(alpha,beta).
 %  
 %  That is, cf_LogGammaRV evaluates the characteristic function cf(t) of
 %  Y = coef(1)*log(X_1)+ ... + coef(N)*log(X_N), where X_i ~
-%  Gamma(alpha_i,beta_i), with the parameters alpha_i > 0 and beta_i > 0.
+%  InverseGamma(alpha_i,beta_i), with the parameters alpha_i > 0 and beta_i 
+%  > 0. 
 %
-%  The characteristic function of Y = log(X), with X ~ Gamma(alpha,beta) is
-%  defined by cf_Y(t) = E(exp(1i*t*Y)) = E(exp(1i*t*log(X))) = E(X^(1i*t)). 
-%  That is, the characteristic function can be derived from expression for
-%  the r-th moment of X, E(X^r) by using (1i*t) instead of r. In
-%  particular, the characteristic function of Y = log(X) is
-%   cf_Y(t) = (1/beta^(1i*t)) .* (gamma(alpha + 1i*t) / gamma(alpha)).
+%  The characteristic function of Y = log(X), with X ~ InvGamma(alpha,beta)
+%  is defined by cf_Y(t) = E(exp(1i*t*Y)) = E(exp(1i*t*log(X))) =
+%  E(X^(1i*t)).  That is, the characteristic function can be derived from
+%  expression for the r-th moment of X, E(X^r) by using (1i*t) instead of
+%  r. In particular, the characteristic function of Y = log(X) is
+%   cf_Y(t) = cf_Z(-t) =  beta^(1i*t).*(gamma(alpha-1i*t) / gamma(alpha)),
+%  where cf_Z(t)  denotes the characteristic function of the log
+%  transformed gamma random variable.
 %
 %  Hence,the characteristic function of Y  = coef(1)*Y1 + ... + coef(N)*YN
 %  is  cf_Y(t) =  cf_Y1(coef(1)*t) * ... * cf_YN(coef(N)*t), where cf_Yi(t)
 %  is evaluated with the parameters alpha(i) and beta(i).
 %
 % SYNTAX
-%  cf = cf_LogGammaRV(t,alpha,beta,coef,n)
+%  cf = cf_LogInverseGammaRV(t,alpha,beta,coef,n)
 %
 % INPUTS:
 %  t     - vector or array of real values, where the CF is evaluated.
@@ -29,59 +34,41 @@ function cf = cf_LogGammaRV(t,alpha,beta,coef,n)
 %  beta  - vector of the 'rate' parameters beta > 0. If empty, default
 %          value is beta = 1.  
 %  coef  - vector of the coefficients of the linear combination of the
-%          logGamma random variables. If coef is scalar, it is assumed
+%          IGamma random variables. If coef is scalar, it is assumed
 %          that all coefficients are equal. If empty, default value is
 %          coef = 1.
 %  n     - scalar convolution coeficient n, such that Z = Y + ... + Y is
 %          sum of n iid random variables Y, where each Y = sum_{i=1}^N
-%          coef(i) * X_i, with X_i ~ logGamma(alpha(i),beta(i)))
-%          independently and identically distributed random variables. If
-%          empty, default value is n = 1.    
+%          coef(i) * GAMMA(k(i),theta(i))) is independently and identically
+%          distributed random variable. If empty, default value is n = 1.   
 %
 % EXAMPLE 1:
-% % CF of a weighted linear combination of independent log-Gamma RVs
+% % CF of a weighted linear combination of independent log-InverseGamma RVs
 %   coef   = [1 2 3 4 5];
 %   weight = coef/sum(coef);
 %   alpha  = 5/2;
 %   beta   = 3/2;
 %   t      = linspace(-20,20,1001);
-%   cf     = cf_LogGammaRV(t,alpha,beta,-weight);
+%   cf     = cf_LogInverseGammaRV(t,alpha,beta,weight);
 %   figure; plot(t,real(cf),t,imag(cf)); grid on;
-%   title('CF of a linear combination of minus log-Gamma RVs')
+%   title('CF of a linear combination of log-InverseGamma RVs')
 %
 % EXAMPLE 2:
-% % PDF/CDF of a linear combination of independent log-Gamma RVs
+% % PDF/CDF of a linear combination of independent  log-InverseGamma RVs
 %   coef   = [1 2 3 4 5];
 %   weight = coef/sum(coef);
 %   alpha  = 5/2;
 %   beta   = 3/2;
-%   cf     = @(t) cf_LogGammaRV(t,alpha,beta,weight);
-%   clear options
-%   options.SixSigmaRule = 8;
-%   result = cf2DistGP(cf,[],[],options);
-%   disp(result)
-%
-% EXAMPLE 3:
-% % PDF/CDF of a log-Gamma (log-ChiSquared) RV with alpha = 5/2, beta = 1/2 
-%   alpha = 5/2;
-%   beta = 1/2;
-%   cf = @(t) cf_LogGammaRV(t,alpha,beta);
+%   cf     = @(t) cf_LogInverseGammaRV(t,alpha,beta,weight);
 %   clear options
 %   options.N = 2^12;
+%   options.SixSigmaRule = 8;
 %   prob = [0.9 0.95 0.99];
 %   result = cf2DistGP(cf,[],prob,options);
 %   disp(result)
-%   qfGamma = exp(result.qf)
 %
 % WIKIPEDIA: 
-%  https://en.wikipedia.org/wiki/Gamma_distribution
-% Wolfram MathWorld:
-%  http://mathworld.wolfram.com/GammaDistribution.html
-%
-% REFERENCES:
-% MATHAI, A.M. (1973). A review of the different techniques used for
-% deriving the exact distributions of multivariate test criteria. Sankhya:
-% The Indian Journal of Statistics, Series A, 39-60.  
+%  https://en.wikipedia.org/wiki/Inverse-gamma_distribution
 
 % (c) 2017 Viktor Witkovsky (witkovsky@gmail.com)
 % Ver.: 02-Jun-2017 12:08:24
@@ -125,8 +112,8 @@ end
 szt = size(t);
 t   = t(:);
 aux = 1i*t*coef;
-aux = gammalog(bsxfun(@plus,aux,alpha))-ones(length(t),1)*gammalog(alpha);
-aux = aux - 1i*t*log(beta);
+aux = gammalog(bsxfun(@plus,-aux,alpha))-ones(length(t),1)*gammalog(alpha);
+aux = aux + 1i*t*log(beta);
 cf  = prod(exp(aux),2);
 cf  = reshape(cf,szt);
 cf(t==0) = 1;
