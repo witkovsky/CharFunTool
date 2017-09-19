922f6a86d08409937f70c2b5fc58235a12ad67d7
Viktor <witkovsky@gmail.com>
2017-09-19 18:38:24 +0200

    Updated ExperimentW001

--- WorkingExperiments/ExperimentW001.m
+++ WorkingExperiments/ExperimentW001.m
@@ -9,8 +9,8 @@ clear
 %% LogNormal distribution
 
 mu = 0;
-sigma = 1;
-cf = @(t)cfX_LogNormal(t,mu,sigma);
+b = 1;
+cf = @(t)cfX_LogNormal(t,mu,b);
 fun = @(t) imag(cf(t))./t;
 A = 1e-12;
 B = 50;
@@ -23,9 +23,12 @@ hold on
 plot(x,logncdf(x,0,1))
 hold off
 
+figure
+plot(coefs,'o-');grid
+
 %% ChiSquare distribution
 
-nu = 3
+nu = 3;
 cf = @(t)cf_ChiSquare(t,nu);
 fun = @(t) imag(cf(t))./t;
 A = 1e-12;
@@ -46,9 +49,9 @@ plot(coefs,'o-');grid
 %% Compound LogNormal-Poisson distribution
 
 mu = 0;
-sigma = 1;
+b = 1;
 lambda = 5;
-cfX = @(t)cfX_LogNormal(t,mu,sigma);
+cfX = @(t)cfX_LogNormal(t,mu,b);
 cf = @(t) cfN_Poisson(t,lambda,cfX);
 fun = @(t) imag(cf(t))./t;
 A = 1e-12;
@@ -58,3 +61,46 @@ x = linspace(0,50,501);
 [FI,coefs] = FourierIntegral_BV(x,fun,A,B);
 CDF = 1-2*real(FI)/pi;
 plot(x,CDF,'.-');grid
+
+figure
+plot(coefs,'o-');grid
+%% Weibull distribution
+
+a = 1.5;
+b = 1;
+cf = @(t)cfX_Weibull(t,a,b);
+fun = @(t) imag(cf(t))./t;
+A = 1e-13;
+B = 70;
+nPts = 30;
+x = linspace(0,5,501);
+
+[FI,coefs] = FourierIntegral_BV(x,fun,A,B,nPts);
+CDF = 1-2*real(FI)/pi;
+plot(x,CDF,'.-');grid
+hold on
+plot(x,wblcdf(x,1/a,b))
+hold off
+
+figure
+plot(coefs,'o-');grid
+
+%% Compound Webull-Poisson distribution
+
+a = 1.5;
+b = 1;
+lambda = 5;
+cfX = @(t)cfX_Weibull(t,a,b);
+cf = @(t) cfN_Poisson(t,lambda,cfX);
+fun = @(t) imag(cf(t))./t;
+A = 1e-12;
+B = 50;
+nPts = 75;
+x = linspace(0,30,501);
+
+tic;[FI,coefs] = FourierIntegral_BV(x,fun,A,B,nPts);toc
+CDF = 1-2*real(FI)/pi;
+plot(x,CDF,'.-');grid
+
+figure
+plot(coefs,'o-');grid
