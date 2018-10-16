07814db567a73b16b369ced77fe7e10a256541d9
Viktor <witkovsky@gmail.com>
2018-10-16 15:05:23 +0200

    NEW Interpolant functions
    NEW Interpolant functions cdf, pdf, qf, rnd

---
+++ CF_Utilities/rndFunction.m
@@ -0,0 +1,72 @@
+function rnd = rndFunction(dim,xOld,cdfOld)
+%rndFunction generates array of size dim of random numbers from the
+%   distribution specified by the CDF from the pre-calculated (known) 
+%   values of xOld, and cdfOld. The evaluation is based on computed
+%   quantiles (from the CDF by using the barycentric interpolation) at
+%   probabilities generated from the uniform distribution.
+%
+% SYNTAX:
+%   rnd = rndFunction(n,xOld,cdfOld)
+%
+% INPUT:
+%  dim     - size of the array of generated random numbers from the
+%            distribution specified by the known CDF, with values cdfOld
+%            that have been pre-computed at xOld. If dim is scalar, size =
+%            [dim,1]. If empty size = [1,1].
+%  xOld    - vector of x-values where the values of CDF are known (have
+%            been pre-computed). Alternatively, if the thisr input
+%            parameter is missing, it is assumed that the second parameter
+%            is the result structure from the cf2DistGP. 
+%  cdfOld  - vector of the known CDF values that have been pre-computed 
+%            at xOld. If this parameter is missing, it is assumed that the
+%            second parameter is the result structure from the cf2DistGP.
+%
+% EXAMPLE 1
+%  cf      = @(t) exp(-t.^2/2);
+%  clear options
+%  options.isPlot = false;
+%  result  = cf2DistGP(cf,[],[],options);
+%  xOld    = result.x;
+%  cdfOld  = result.cdf;
+%  rnd     = @(dim) rndFunction(dim,xOld,cdfOld);
+%  dim = [10000,1];
+%  hist(rnd(dim),51);
+%
+% EXAMPLE 2
+%  cf      = @(t) exp(-t.^2/2);
+%  clear options
+%  options.isPlot = false;
+%  result  = cf2DistGP(cf,[],[],options);
+%  rnd     = @(dim) rndFunction(dim,result);
+%  dim = [10000,1];
+%  hist(rnd(dim),51);
+
+% (c) Viktor Witkovsky (witkovsky@gmail.com)
+% Ver.: 16-Oct-2018 13:46:35
+
+%% CHECK THE INPUT PARAMETERS
+narginchk(2, 3);
+
+if nargin < 3
+    if isstruct(xOld)
+        result = xOld;
+        xOld   = result.x;
+        cdfOld = result.cdf;
+    else
+        error('Missing Inputs')
+    end
+end
+
+if isempty(dim)
+    dim = [1,1];   
+end
+
+if isscalar(dim)
+    dim = [dim,1];
+end
+
+prob = rand(dim(1)*dim(2),1);
+rnd  = qfFunction(prob,xOld,cdfOld);
+rnd  = reshape(rnd,dim);
+
+end
