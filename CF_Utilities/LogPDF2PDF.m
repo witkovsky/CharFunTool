6e1971b4086b3dc855863e6547e02cd84ab4a263
Viktor <witkovsky@gmail.com>
2018-10-18 10:25:26 +0200

    Update CF_Utilities/LogPDF2PDF.m

--- CF_Utilities/LogPDF2PDF.m
+++ CF_Utilities/LogPDF2PDF.m
@@ -52,9 +52,39 @@ function [x,pdf] = LogPDF2PDF(LogX,LogPDF)
 %  result = cf2DistGP(cf,[],[],options);
 %  [x,pdf] = LogPDF2PDF(result);
 %  figure; plot(x,pdf)
+%
+% EXAMPLE 4:
+%  % PDF/CDF/QF/RND of Wilks Lambda (p=5,n=10,q=3) from LOG-TRANSFORMED RV
+%  rng(101);
+%  p    = 5;
+%  n    = 10;
+%  q    = 3;
+%  cf   = @(t) cf_LogRV_WilksLambda(t,p,n,q);
+%  pts  = log(ChebPoints(2^9,[0,1]));
+%  clear options
+%  options.isPlot = 0;
+%  options.xMax = 0;
+%  options.xMin = pts(2);
+%  xx        = pts(2:end-1);
+%  result    = cf2DistGP(cf,xx,[],options);
+%  [xxx,cdf] = LogCDF2CDF(result);
+%  [~,pdf]   = LogPDF2PDF(result);
+%  PDF       = @(x) PDFinterp(x,[0;xxx(:);1],[0;pdf(:);0])
+%  CDF       = @(x) CDFinterp(x,[0;xxx(:);1],[0;cdf(:);1])
+%  QF        = @(prob) QFinterp(prob,[0;xxx(:);1],[0;cdf(:);1])
+%  RND       = @(dim) RNDinterp(dim,[0;xxx(:);1],[0;cdf(:);1])
+%  % Plot of PDF/CDF/QF/RND 
+%  x   = linspace(0,1,201);
+%  p   = linspace(0,1,201);
+%  dim = [100000,1];
+%  subplot(2,2,1);plot(x,CDF(x))
+%  subplot(2,2,2);plot(x,PDF(x))
+%  subplot(2,2,3);plot(p,QF(p))
+%  subplot(2,2,4);histogram(RND(dim),100,'Normalization','pdf');
+%  hold on;plot(x,PDF(x));hold off
 
 % (c) Viktor Witkovsky (witkovsky@gmail.com)
-% Ver.: 17-Oct-2018 16:55:59
+% Ver.: 18-Oct-2018 10:03:08
 
 %% ALGORITHM
 %  [x,pdf] = LogPDF2PDF(LogX,LogPDF)
