daf43ea238a9a90c24ffa3ea24005f3f88e03aa0
Viktor <witkovsky@gmail.com>
2018-12-03 23:51:04 +0100

    Update cf2PDF.m

--- CF_InvAlgorithms/cf2PDF.m
+++ CF_InvAlgorithms/cf2PDF.m
@@ -6,7 +6,7 @@ function [pdf,result] = cf2PDF(cf,x,options)
 %  fast root-finding algorithm) and subsequent application of the
 %  convergence acceleration techniques for computing the limit of the
 %  resulted alternating series, for more details see, e.g., Cohen et al.
-%  (2000), Sidi (2011), Sidi (2017).
+%  (2000), Sidi (2011).
 %
 % NOTE
 %  The algorithms cf2CDF/cf2PDF/cf2Qf are suggested as alternative to the
