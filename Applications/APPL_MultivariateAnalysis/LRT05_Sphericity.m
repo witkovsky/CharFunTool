1e29d321ff8fb1a791227de4f510c685465dff65
Viktor <witkovsky@gmail.com>
2018-12-09 16:05:46 +0100

    Update LRT05_Sphericity.m

--- Applications/APPL_MultivariateAnalysis/LRT05_Sphericity.m
+++ Applications/APPL_MultivariateAnalysis/LRT05_Sphericity.m
@@ -1,23 +1,31 @@
 function [pval,result] = LRT05_Sphericity(W,n,p,options)
 %% LRT05_Sphericity computes p-value of the log-transformed LRT statistic,
-%  W = -log(Lambda), for testing the null hypothesis on the shape of the
-%  unknown covariance matrix Sigma of p-dimensional normal distribution,
-%  and/or its null distribution CF/PDF/CDF. In particular, let X ~
-%  N_p(mu,Sigma), then, the null hypothesis is given as  
-%    H0: Sigma = sigma^2 I_p (sigma^2 unspecified).
-%  Here, the LRT test statistic is given by  
-%    Lambda = ( det(S) / trace((1/p)*S)^p )^{n/2},
-%  where S is MLE of Sigma based on sample size n from X ~ N_p(mu,Sigma).
+%  for testing the null hypothesis on the shape of the unknown covariance
+%  matrix Sigma of p-dimensional normal distribution, and/or its null
+%  distribution CF/PDF/CDF.  
 %
-%  Under null hypothesis, distribution of the test statistic Lambda is
-%    Lambda ~  prod_{j=2}^{p} (B_j)^{n/2}, 
+%  Let X ~ N_p(mu,Sigma) with p >= 2. Then the null hypothesis is given as  
+%    H0: Sigma = sigma^2 I_p (sigma^2 > 0 is unspecified).
+%  Here, the LRT test statistic is given by  
+%    LRT = Lambda^(n/2) = (det(S) / trace((1/p)*S)^p)^(n/2),
+%  and the modified LRT is defined as
+%    MLRT = Lambda = det(S) / trace((1/p)*S)^p,
+%  where Lambda = det(S) / trace((1/p)*S)^p, where S is MLE of Sigma based
+%  on sample size n from X ~ N_p(mu,Sigma). 
+%
+%  Under null hypothesis, distribution of the test statistic LRT is
+%    LRT ~  prod_{j=2}^{p} (B_j)^{n/2}, 
 %  with B_j ~ Beta((n-j)/2,(j-1)/p + (j-1)/2), where j = [2,...,p]. Here
-%  we assume that n > p.  
+%  we assume that n > p. 
 %
 %  Hence, the exact characteristic function of the null distribution of
-%  minus log-transformed LRT statistic Lambda, say W = -log(Lambda) is
+%  minus log-transformed LRT statistic Lambda, say W = -log(LRT) is
 %  given by 
 %   cf = @(t) cf_LogRV_Beta(-(n/2)*t, (n-j)/2, (j-1)/p + (j-1)/2),
+%  where j = [2,..., p]'.
+%  Similarly, the exact characteristic function of the null distribution of
+%  minus log-transformed modified LRT statistic, say W = -log(MLRT) is
+%   cf = @(t) cf_LogRV_Beta(-t, (n-j)/2, (j-1)/p + (j-1)/2),
 %  where j = [2,..., p]'.
 %
 % SYNTAX:
@@ -34,10 +42,10 @@ function [pval,result] = LRT05_Sphericity(W,n,p,options)
 %  options - option structure, for more details see cf2DistGP. Moreover,
 %            x    - set vector of values where PDF/CDF is evaluated
 %            prob - set vector of probabilities for the quantiles.
-%            coef - set arbitrary multiplicator of the argument t of
-%            the characteristic function. If empty, default value is -n/2
-%            (standard value for minus log-transform of LRT). Possible
-%            alternative is e.g. coef = -1, leading to W = -(2/n)*log(LRT).
+%            type - specifies the type of the LRT test statistic: type =
+%            'standard' with W = -(n/2)*log(Lambda), or type = 'modified'
+%            with W = -log(Lambda). If type is empty or missing, default
+%            value is type  = 'modified'.
 %
 % EXAMPLE: (LRT for testing hypothesis on sphericity of covariance matrix)
 % % Null distribution of the minus log-transformed LRT statistic
@@ -60,8 +68,8 @@ function [pval,result] = LRT05_Sphericity(W,n,p,options)
 %       criteria by numerical inversion of their characteristic functions.
 %       arXiv preprint arXiv:1801.02248, 2018.   
 
-% (c) 2018 Viktor Witkovsky (witkovsky@gmail.com)
-% Ver.: 20-Jan-2018 12:43:15
+% (c) Viktor Witkovsky (witkovsky@gmail.com)
+% Ver.: 09-Dec-2018 15:34:58
 
 %% ALGORITHM
 % [pval,result] = LRT05_Sphericity(W,n,p,options)
@@ -87,37 +95,29 @@ if ~isfield(options, 'xMin')
     options.xMin = 0;
 end
 
+if ~isfield(options, 'type')
+    options.type = [];
+end
+
+type = options.type;
+
 %% 
-if n <= p 
-error('Sample size n is too small')
+if isempty(type)
+    type = 'standard';
 end
 
+cf = @(t) cfTest_Sphericity(t,n,p,type);
 
-% CHARACTERISTIC FUNCTION CF
-coef = options.coef;
-if isempty(coef)
-    coef = -n/2;  % Set this option for using with W = -log(LRT)
-    % coef = -1;  % Set this options for using normalized -log(LRT^(2/n))
-end
-ind   = (2:p)';
-alpha = (n-ind)/2;
-beta  = (ind-1)/p + (ind-1)/2;
-cf    = @(t)cf_LogRV_Beta(t,alpha,beta,coef);
-
-% Evaluate the p-value and PDF/CDF/QF of the log-transformed LRT statistic
+% Evaluate p-value or PDF/CDF/QF
 if ~isempty(W)
     % P-VALUE
     options.isPlot = false;
     result = cf2DistGP(cf,W,[],options);
     pval   = 1-result.cdf;
 else
-    % DISTRIBUTION of Lambda PDF/CDF
+    % PDF/CDF/QF otf the test statistic
     pval   = [];
     result = cf2DistGP(cf,options.x,options.prob,options);
 end
-
-% Save the parameters of the used beta distributions
-result.alpha = alpha;
-result.beta  = beta;
 
 end
