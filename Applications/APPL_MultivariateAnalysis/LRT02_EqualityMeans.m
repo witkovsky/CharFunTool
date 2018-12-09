eb6054fa6c7cc5cb3d2f746349b589f4360ba4e7
Viktor <witkovsky@gmail.com>
2018-12-09 16:05:18 +0100

    Update LRT02_EqualityMeans.m

--- Applications/APPL_MultivariateAnalysis/LRT02_EqualityMeans.m
+++ Applications/APPL_MultivariateAnalysis/LRT02_EqualityMeans.m
@@ -2,29 +2,36 @@ function [pval,result] = LRT02_EqualityMeans(W,n,p,q,options)
 %% LRT02_EqualityMeans computes p-value of the log-transformed LRT statistic,
 %  W = -log(Lambda), for testing the null hypothesis of equality of means
 %  resp. means vectors (under normality assumptions) of q (q>1)
-%  p-dimensional populations, and/or its null distribution CF/PDF/CDF. In
-%  particular, let X_k ~ N_p(mu_k,Sigma), with common covariance matrix
-%  Sigma for all k = 1,...,q. We want to test the hypothesis that the mean
-%  vectors mu_k are equal for all X_k, k = 1,...,q. Then, the null
-%  hypothesis is given as  
+%  p-dimensional populations, and/or its null distribution CF/PDF/CDF. 
+%
+%  Let X_k ~ N_p(mu_k,Sigma) are p-dimensional random vectors with common
+%  covariance matrix Sigma for all k = 1,...,q. We want to test the
+%  hypothesis that the mean vectors mu_k are equal for all X_k, k =
+%  1,...,q. Then, the null hypothesis is given as
 %    H0: mu_1 = ... = mu_q, 
 %  i.e. the mean vectors are equal in all q populations. Here, the LRT test
 %  statistic is given by  
-%    Lambda = ( det(E) / det(E+H) )^{n/2},
-%  where E = sum_{k=1}^q sum_{j=1}^{n_k} (X_{kj} - bar{X}_k)'*(X_{kj} -
-%  bar{X}_k) with E ~ Wishart(n-q,Sigma), and H = sum_{k=1}^q (bar{X}_k -
-%  bar{X})'*(bar{X}_k - bar{X}) with H ~ Wishart(q-1,Sigma) based on n =
-%  n_1 + ... + n_q  samples from the q p-dimensional populations. 
+%    LRT = Lambda^(n/2) = ( det(E) / det(E+H) )^{n/2},
+%  and the modified LRT is defined as
+%    MLRT = Lambda = det(E) / det(E+H)
+%  where Lambda = det(E) / det(E+H) with E = sum_{k=1}^q sum_{j=1}^{n_k}
+%  (X_{kj} - bar{X}_k)'*(X_{kj} - bar{X}_k) with E ~ Wishart(n-q,Sigma),
+%  and H = sum_{k=1}^q (bar{X}_k - bar{X})'*(bar{X}_k - bar{X}) with H ~
+%  Wishart(q-1,Sigma) based on n = n_1 + ... + n_q  samples from the q
+%  p-dimensional populations.
 %
-%  Under null hypothesis, distribution of the test statistic Lambda is
-%    Lambda ~  prod_{j=1}^{p} (B_j)^{n/2}, 
-%  with B_j ~ Beta((n-q-j+1)/2,(q-1)/2). Here we assume that n >
-%  min(p+q-1). 
+%  Under null hypothesis, distribution of the LRT statistic is
+%    LRT ~  prod_{j=1}^{p} (B_j)^{n/2}, 
+%  with B_j ~ Beta((n-q-j+1)/2,(q-1)/2). Here we assume n > p+q-1. 
 %
 %  Hence, the exact characteristic function of the null distribution of
-%  minus log-transformed LRT statistic Lambda, say W = -log(Lambda) is
+%  minus log-transformed LRT statistic, say W = -log(LRT) is
 %  given by 
 %   cf = @(t) cf_LogRV_Beta(-(n/2)*t, (n-q-j+1)/2, (q-1)/2),
+%  where j = [1, 2, ..., p]'.
+%  Similarly, the exact characteristic function of the null distribution of
+%  minus log-transformed modified LRT statistic, say W = -log(MLRT) is
+%   cf = @(t) cf_LogRV_Beta(-t, (n-q-j+1)/2, (q-1)/2),
 %  where j = [1, 2, ..., p]'.
 %
 % SYNTAX:
@@ -42,10 +49,10 @@ function [pval,result] = LRT02_EqualityMeans(W,n,p,q,options)
 %  options - option structure, for more details see cf2DistGP. Moreover,
 %            x    - set vector of values where PDF/CDF is evaluated
 %            prob - set vector of probabilities for the quantiles.
-%            coef - set arbitrary multiplicator of the argument t of
-%            the characteristic function. If empty, default value is -n/2
-%            (standard value for minus log-transform of LRT). Possible
-%            alternative is e.g. coef = -1, leading to W = -(2/n)*log(LRT).
+%            type - specifies the type of the LRT test statistic: type =
+%            'standard' with W = -(n/2)*log(Lambda), or type = 'modified'
+%            with W = -log(Lambda). If type is empty or missing, default
+%            value is type  = 'modified'.
 %
 % EXAMPLE: (LRT for testing hypothesis on equality of means)
 % % Null distribution of the minus log-transformed LRT statistic
@@ -54,7 +61,8 @@ function [pval,result] = LRT02_EqualityMeans(W,n,p,q,options)
 % q = 5;            % number of populations 
 % W = [];           % observed value of W = -log(Lambda)
 % clear options;
-% % options.coef = -1;
+% options.type = 'standard';
+% % options.type = 'modified';
 % options.prob = [0.9 0.95 0.99];
 % [pval,result] = LRT02_EqualityMeans(W,n,p,q,options)
 %
@@ -69,8 +77,8 @@ function [pval,result] = LRT02_EqualityMeans(W,n,p,q,options)
 %       criteria by numerical inversion of their characteristic functions.
 %       arXiv preprint arXiv:1801.02248, 2018.   
 
-% (c) 2018 Viktor Witkovsky (witkovsky@gmail.com)
-% Ver.: 20-Jan-2018 12:43:15
+% (c) Viktor Witkovsky (witkovsky@gmail.com)
+% Ver.: 09-Dec-2018 15:34:58
 
 %% ALGORITHM
 % [pval,result] = LRT02_EqualityMeans(W,n,p,q,options)
@@ -96,38 +104,29 @@ if ~isfield(options, 'xMin')
     options.xMin = 0;
 end
 
+if ~isfield(options, 'type')
+    options.type = [];
+end
+
+type = options.type;
+
 %% 
-N = p + q -1;
-if n <= N 
-error('Sample size n is too small')
+if isempty(type)
+    type = 'standard';
 end
 
+cf = @(t) cfTest_EqualityMeans(t,n,p,q,type);
 
-% CHARACTERISTIC FUNCTION CF
-coef = options.coef;
-if isempty(coef)
-    coef = -n/2;  % Set this option for using with W = -log(LRT)
-    % coef = -1;  % Set this options for using normalized -log(LRT^(2/n))
-end
-ind   = (1:p)';
-alpha = (n-q-ind+1)/2;
-beta  = (q-1)/2;
-cf    = @(t)cf_LogRV_Beta(t,alpha,beta,coef);
-
-% Evaluate the p-value and PDF/CDF/QF of the log-transformed LRT statistic
+% Evaluate p-value or PDF/CDF/QF
 if ~isempty(W)
     % P-VALUE
     options.isPlot = false;
     result = cf2DistGP(cf,W,[],options);
     pval   = 1-result.cdf;
 else
-    % DISTRIBUTION of Lambda PDF/CDF
+    % PDF/CDF/QF otf the test statistic
     pval   = [];
     result = cf2DistGP(cf,options.x,options.prob,options);
 end
-
-% Save the parameters of the used beta distributions
-result.alpha = alpha;
-result.beta  = beta;
 
 end
