09b66f979f561bd60699872eb094c98322abf15d
Viktor <witkovsky@gmail.com>
2019-09-18 16:33:02 +0200

    Update GammaMultiLog.m
    Corrected errror in computation of  GammaMultiLog

--- CF_Utilities/GammaMultiLog.m
+++ CF_Utilities/GammaMultiLog.m
@@ -3,9 +3,9 @@ function f = GammaMultiLog(z,p,funmode)
 % order p, the argument z may be complex and of any size. 
 %
 % Multivariate gamma function of order p, say gamma_p(z), is defined as
-%  gamma_p(z) = pi^(p*(p-1)/2) * prod_{j=1}^p gamma(z-(j-1)/2).
+%  gamma_p(z) = pi^(p*(p-1)/4) * prod_{j=1}^p gamma(z-(j-1)/2).
 % Hence, the logarithm of multivariate gamma is given by
-%  log(gamma_p(z))=(p*(p-1)/2)*log(pi) + sum_{j=1}^p log(gamma(z-(j-1)/2))
+%  log(gamma_p(z))=(p*(p-1)/4)*log(pi) + sum_{j=1}^p log(gamma(z-(j-1)/2))
 %
 % SYNTAX
 %   f = GammaMultiLog(z,p,funmode)
@@ -31,7 +31,7 @@ function f = GammaMultiLog(z,p,funmode)
 %  f = GammaMultiLog(z,p,funmode)
 
 % Viktor Witkovsky (witkovsky@gmail.com)
-% Ver.: 17-Aug-2018 19:45:37
+% Ver.: 18-Sep-2019 16:26:09
 
 %% FUNCTION
 %  f = GammaMultiLog(z,p,funmode)
@@ -57,7 +57,8 @@ end
 sz = size(z);
 z  = z(:);
 
-f = (p*(p-1)/2)*log(pi);
+% This was corrected on September 18, 2019 (from f = (p*(p-1)/2)*log(pi))
+f = (p*(p-1)/4)*log(pi);
 
 for j = 1:p
     f = f + GammaLog(z - (j-1)/2);
