0b7b44dbbd2cca38059c15452d69b37ca9084736
Viktor <witkovsky@gmail.com>
2019-09-21 13:07:31 +0200

    Half-Normal and Folded-Normal
    NEW CF Half-Normal and Folded-Normal

---
+++ CF_Repository/cf_FoldedNormal.m
@@ -0,0 +1,143 @@
+function cf = cf_FoldedNormal(t,mu,sigma,coef,niid)
+%cf_FoldedNormal 
+%  Characteristic function of a linear combination (resp. convolution) of
+%  independent Folded-Normal random variables.     
+%   
+%  The Folded-Normal distribution is a continuous probability distribution
+%  for positive-valued random variables which is proportional to the
+%  non-central chi-distribution with one degree of freedom. 
+% 
+%  In particular, if U ~ N(mu/sigma,1) then X = sigma*|U| has Folded-Normal
+%  distribution with the parameters mu and sigma, X ~ FN(mu,sigma). That
+%  is, the random variable X is multiple of the random variable |U| which
+%  has non-central chi-distribution with one degree of freedom and the
+%  noncentrality parameter delta = mu/sigma.   
+%
+%  cf_FoldedNormal evaluates the characteristic function cf(t) of Y =
+%  sum_{i=1}^N coef_i * X_i, where X_i ~ Folded-Normal(sigma_i) are
+%  inedependent Folded-Normal RVs with the scale parameters sigma_i > 0,
+%  for i  = 1,...,N. 
+%
+%  The characteristic function of X ~ FN(mu,sigma) is defined by
+%   cf_FoldedNormal(t) = cf_ChiNC(sigma*t,df=1,delta=mu/sigma), 
+%  where cf_ChiNC(t,df,delta) denotes the characteristic function of the
+%  non-central Chi-distribution with df degrees of freedom and the
+%  non-centrality parameter delta. Hence, the characteristic 
+%  function of Y is 
+%   cf(t) = Prod ( cf_FoldedNormal(coef_i*t,sigma_i) )
+%         = Prod ( cf_FoldedNormal(coef_i*sigma_i*t,1) )
+%
+%  Alternatively, the characteristic function of X ~ FN(mu,sigma) can be
+%  expressed by 
+%   cf_FoldedNormal(t) = exp(-sigma^2*t^2/2 + 1i*mu*t) 
+%                        * (1/2)*(1-erf((-mu/sigma+1i*sigma*t)/sqrt(2)))
+%                      + exp(-sigma^2*t^2/2 - 1i*mu*t) 
+%                        * (1/2)*(1-erf((mu/sigma+1i*sigma*t)/sqrt(2)))
+%  where erf(z) denotes the complex error function. See also erfZX.m
+%
+% SYNTAX:
+%  cf = cf_FoldedNormal(t,mu,sigma,coef,niid)
+% 
+% INPUTS:
+%  t     - vector or array of real values, where the CF is evaluated.
+%  mu    - vector of the location parameters of the Folded-Normal random
+%          variables. If mu is scalar, it is assumed that all location
+%          parameters are equal. If empty, default value is mu = 0. 
+%  sigma - vector of the scale parameters of the Folded-Normal random
+%          variables. If sigma is scalar, it is assumed that all scale
+%          parameters are equal. If empty, default value is sigma = 1. 
+%  coef  - vector of the coefficients of the linear combination of the
+%          Folded-Normal random variables. If coef is scalar, it is assumed
+%          that all coefficients are equal. If empty, default value is
+%          coef = 1.
+%  niid  - scalar convolution coeficient niid, such that Z = Y + ... + Y is
+%          sum of niid iid random variables Y, where each Y = sum_{i=1}^N
+%          coef(i) * log(X_i) is independently and identically distributed
+%          random variable. If empty, default value is niid = 1.   
+%
+% WIKIPEDIA:
+%  https://en.wikipedia.org/wiki/Folded_normal_distribution
+%  https://en.wikipedia.org/wiki/Noncentral_chi_distribution
+%
+% NOTES (from Wikipedia)
+%  The folded normal distribution is a probability distribution related to
+%  the normal distribution. Given a normally distributed random variable X
+%  with mean ? and variance ?2, the random variable Y = |X| has a folded
+%  normal distribution. Such a case may be encountered if only the
+%  magnitude of some variable is recorded, but not its sign. The
+%  distribution is called "folded" because probability mass to the left of
+%  the x = 0 is folded over by taking the absolute value. In the physics of
+%  heat conduction, the folded normal distribution is a fundamental
+%  solution of the heat equation on the upper plane (i.e. a heat kernel).        
+%
+% EXAMPLE 1:
+% % CF of the distribution of Folded-Normal RV with mu = 1 and sigma = 3
+%   mu    = 1;
+%   sigma = 3;
+%   t     = linspace(-10,10,501);
+%   cf    = cf_FoldedNormal(t,mu,sigma);
+%   figure; plot(t,real(cf),t,imag(cf));grid on
+%   title('CF of the Folded-Normal RV with mu = 1 and sigma = 3')
+%
+% EXAMPLE 2: 
+% % CF of a linear combination of independent Folded-Normal RVs
+%   mu    = [1 1 1 1 1];
+%   sigma = [1 2 3 4 5];
+%   coef  = [1 1 1 1 1];
+%   t     = linspace(-5,5,501);
+%   cf    = cf_FoldedNormal(t,mu,sigma,coef);
+%   figure; plot(t,real(cf),t,imag(cf));grid on
+%   title('CF of a linear combination of independent Folded-Normal RVs')
+%
+% EXAMPLE 3:
+% % PDF/CDF of a linear combination of independent Folded-Normal RVs
+%   mu    = [1 1 1 1 1];
+%   sigma = [1 2 3 4 5];
+%   coef  = [1 1 1 1 1];
+%   cf    = @(t) cf_FoldedNormal(t,mu,sigma,coef);
+%   clear options
+%   options.N = 2^10;
+%   options.xMin = 0;
+%   x = linspace(0,10,201);
+%   prob = [0.9 0.95 0.975 0.99];
+%   result = cf2DistGP(cf,x,prob,options);
+%
+% See also: cf_ChiNC, erfZX
+
+% (c) Viktor Witkovsky (witkovsky@gmail.com)
+% Ver.: 21-Sep-2019 12:33:09
+
+%% ALGORITHM
+%  cf = cf_FoldedNormal(t,mu,sigma,coef,niid)
+
+%% CHECK THE INPUT PARAMETERS
+narginchk(1, 5);
+if nargin < 5, niid  = []; end
+if nargin < 4, coef  = []; end
+if nargin < 3, sigma = []; end
+if nargin < 2, mu = []; end
+
+if isempty(mu)
+    mu = 0;
+end
+
+if isempty(sigma)
+    sigma = 1;
+end
+
+if isempty(coef) 
+    coef = 1;
+end
+
+% Check/set equal dimensions for the vectors coef and sigma 
+[errorcode,coef,sigma,mu] = distchck(3,coef(:),sigma(:),mu(:));
+if errorcode > 0
+        error(message('InputSizeMismatch'));
+end
+
+% CF of the linear combination of the Folded-Normal RVs (by using ChiNC) 
+df = 1;
+delta = mu./sigma;
+cf = cf_ChiNC(t,df,sigma.*coef,delta,niid);
+
+end
---
+++ CF_Repository/cf_HalfNormal.m
@@ -0,0 +1,127 @@
+function cf = cf_HalfNormal(t,sigma,coef,niid)
+%cf_HalfNormal 
+%  Characteristic function of a linear combination (resp. convolution) of
+%  independent Half-Normal random variables.     
+%   
+%  The Half-Normal distribution is a continuous probability distribution
+%  for positive-valued random variables which is proportional to the
+%  chi-distribution with one degree of freedom. 
+% 
+%  In particular, if Z ~ N(0,1) then X = sigma*|Z| has Half-Normal
+%  distribution with parameter sigma, X ~ HN(sigma). That is, the random
+%  variable X is multiple of the random variable |Z| which has
+%  chi-distribution with one degree of freedom.  
+%
+%  cf_HalfNormal evaluates the characteristic function cf(t) of Y =
+%  sum_{i=1}^N coef_i * X_i, where X_i ~ Half-Normal(sigma_i) are
+%  inedependent Half-Normal RVs with the scale parameters sigma_i > 0, for
+%  i  = 1,...,N. 
+%
+%  The characteristic function of X ~ HN(sigma) is defined by
+%   cf_HalfNormal(t) = cf_Chi(sigma*t,df=1), 
+%  where cf_Chi(t,df) denotes the characteristic function of the Chi
+%  distribution with df degrees of freedom. Hence, the characteristic
+%  function of Y is 
+%   cf(t) = Prod ( cf_HalfNormal(coef_i*t,sigma_i) )
+%         = Prod ( cf_HalfNormal(coef_i*sigma_i*t,1) )
+%
+%  Alternatively, the characteristic function of X ~ HN(sigma) can  be
+%  expressed by 
+%   cf_HalfNormal(t) = exp(-sigma^2*t^2/2) * (1-erf(-1i*sigma*t/sqrt(2))),
+%  where erf(z) denotes the complex error function. See also erfZX.m
+%
+% SYNTAX:
+%  cf = cf_HalfNormal(t,sigma,coef,niid)
+% 
+% INPUTS:
+%  t     - vector or array of real values, where the CF is evaluated.
+%  sigma - vector of the scale parameters of the Half-Normal random
+%          variables. If sigma is scalar, it is assumed that all scale
+%          parameters are equal. If empty, default value is sigma = 1. 
+%  coef  - vector of the coefficients of the linear combination of the
+%          Half-Normal random variables. If coef is scalar, it is assumed
+%          that all coefficients are equal. If empty, default value is
+%          coef = 1.
+%  niid  - scalar convolution coeficient niid, such that Z = Y + ... + Y is
+%          sum of niid iid random variables Y, where each Y = sum_{i=1}^N
+%          coef(i) * log(X_i) is independently and identically distributed
+%          random variable. If empty, default value is niid = 1.   
+%
+% WIKIPEDIA:
+%  https://en.wikipedia.org/wiki/Half-normal_distribution
+%  https://en.wikipedia.org/wiki/Chi_distribution
+%
+% NOTES (from Wikipedia)
+%  - The Half-Normal distribution is a special case of the Folded-Normal
+%    distribution with its location parameter mu = 0. 
+%  - The Half-Normal distribution is a special case of the generalized
+%    gamma distribution with d = 1, p = 2, a = sqrt(2)*sigma.  
+%  - The Half-Normal distribution coincides with a zero-mean normal
+%    distribution truncated from below at zero (see truncated normal
+%    distribution).
+%  - If Y has a Half-Normal distribution, then (Y/sigma)^2 has a chi-square
+%    distribution with 1 degree of freedom, i.e. Y/sigma has a
+%    chi-distribution with 1 degree of freedom.  
+%
+% EXAMPLE 1:
+% % CF of the distribution of Half-Normal RV with sigma = 3
+%   sigma = 3;
+%   t     = linspace(-5,5,501);
+%   cf    = cf_HalfNormal(t,sigma);
+%   figure; plot(t,real(cf),t,imag(cf));grid on
+%   title('Characteristic function of the Half-Normal RV with sigma = 3')
+%
+% EXAMPLE 2: 
+% % CF of a linear combination of independent Half-Normal RVs
+%   sigma = [1 2 3 4 5];
+%   coef  = [1 1 1 1 1];
+%   t     = linspace(-1,1,501);
+%   cf    = cf_HalfNormal(t,sigma,coef);
+%   figure; plot(t,real(cf),t,imag(cf));grid on
+%   title('CF of a linear combination of independent Half-Normal RVs')
+%
+% EXAMPLE 3:
+% % PDF/CDF of a linear combination of independent Half-Normal RVs
+%   sigma = [1 2 3 4 5];
+%   coef  = [1 1 1 1 1];
+%   cf    = @(t) cf_HalfNormal(t,sigma,coef);
+%   clear options
+%   options.N = 2^10;
+%   options.xMin = 0;
+%   x = linspace(5,40,201);
+%   prob = [0.9 0.95 0.975 0.99];
+%   result = cf2DistGP(cf,x,prob,options);
+%
+% See also: cf_Chi, erfZX
+
+% (c) Viktor Witkovsky (witkovsky@gmail.com)
+% Ver.: 21-Sep-2019 12:33:09
+
+%% ALGORITHM
+%  cf = cf_HalfNormal(t,sigma,coef,niid)
+
+%% CHECK THE INPUT PARAMETERS
+narginchk(1, 4);
+if nargin < 4, niid  = []; end
+if nargin < 3, coef  = []; end
+if nargin < 2, sigma = []; end
+
+if isempty(sigma)
+    sigma = 1;
+end
+
+if isempty(coef) 
+    coef = 1;
+end
+
+% Check/set equal dimensions for the vectors coef and sigma 
+[errorcode,coef,sigma] = distchck(2,coef(:),sigma(:));
+if errorcode > 0
+        error(message('InputSizeMismatch'));
+end
+
+% CF of the linear combination of the Half-Normal RVs (expressed by using Chi)
+df = 1;
+cf = cf_Chi(t,df,sigma.*coef,niid);
+
+end
