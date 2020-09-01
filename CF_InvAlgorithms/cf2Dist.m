71731369913d83d1a73ad386cb4d64dc1052e7f0
Viktor <witkovsky@gmail.com>
2020-09-01 18:08:15 +0200

    Update cf2Dist.m

--- CF_InvAlgorithms/cf2Dist.m
+++ CF_InvAlgorithms/cf2Dist.m
@@ -18,13 +18,15 @@ function [result,cdf,pdf,qf] = cf2Dist(cf,x,prob,options,algorithm)
 %  algorithm - selected algorithm for numerical inversion of the
 %              characteristic function. Currently available inversion
 %              algorithms include: 
-%              'cf2DistBTAV' (Bromwich-Talbot-Abate-Valko method), 
-%              'cf2DistBV', (Bakhvalov-Vasilieva method)
-%              'cf2DistFFT', (Fast Fourier Transform algorithm method)
-%              'cf2DistGPT', (Gil-Pelaez with Trapezoidal quadrature)
-%              'cf2DistGPA' (Gil-Pelaez with adaptive Gauss-Kronrod
-%               quadrature and convergence acceleration techniques).  
-%              If empty, default value is algorithm = 'cf2DistGPA'.
+%              'cf2DistBTAV'  (Bromwich-Talbot-Abate-Valko method), 
+%              'cf2DistBV',   (Bakhvalov-Vasilieva method)
+%              'cf2DistFFT',  (Fast Fourier Transform algorithm method)
+%              'cf2DistGP',   (Gil-Pelaez with Trapezoidal quadrature)
+%              'cf2DistGPT',  (Gil-Pelaez with Trapezoidal quadrature)
+%              'cf2DistGPA'   (Gil-Pelaez with adaptive Gauss-Kronrod
+%                              quadrature and convergence acceleration 
+%                              techniques).  
+%              If empty, default value is algorithm = 'cf2DistGP'.
 % OUTPUT:
 %  result   - structure with CDF/PDF/QF and further details,
 %  cdf      - vector of CDF values evaluated at x,
@@ -91,6 +93,9 @@ function [result,cdf,pdf,qf] = cf2Dist(cf,x,prob,options,algorithm)
 %           cf2QF 
 
 % (c) Viktor Witkovsky (witkovsky@gmail.com)
+% Ver.: 01-Sep-2020 13:25:21
+%
+% Revision history:
 % Ver.: 02-Dec-2018 20:23:52
 
 %% ALGORITHM
@@ -102,7 +107,7 @@ if nargin < 3, prob = []; end
 if nargin < 2, x = []; end
 
 if isempty(algorithm)
-    [result,cdf,pdf,qf] = cf2DistGPA(cf,x,prob,options);
+    [result,cdf,pdf,qf] = cf2DistGP(cf,x,prob,options);
 else
     [result,cdf,pdf,qf] = eval([algorithm,'(cf,x,prob,options)']);
 end
