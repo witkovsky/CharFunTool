1a8ccca46818de8a2fbf36758add54d52d5069b6
Viktor <witkovsky@gmail.com>
2021-05-28 15:05:14 +0200

    Update InterpChebValues.m

--- CF_Utilities/InterpChebValues.m
+++ CF_Utilities/InterpChebValues.m
@@ -1,17 +1,17 @@
 function [fun,x,domain] = InterpChebValues(values,x,domain)
 % InterpChebValues Evaluates the function fun by using Chebyshev
 %   polynomial approximation specified by the function values evaluated at
-%   Chebyshev points with specified domain.   
+%   Chebyshev points with specified domain.
 %
 % SYNTAX:
 %   [fun,x,domain] = InterpChebValues(values,x,domain)
 %
 % EXAMPLE1 (Interpolation by Chebyshev polynomial approximation of Sine)
 %   n      = 2^5+1;
-%   nodes  = ChebPoints(n,[-pi,pi]);
-%   values = sin(nodes);
-%   x      = linspace(-pi,pi)';
 %   domain = [-pi,pi];
+%   nodes  = ChebPoints(n,domain);
+%   values = sin(nodes);
+%   x      = linspace(domain(1),domain(2))';
 %   fun    = InterpChebValues(values,x,domain);
 %   figure; plot(x,fun,'.-'); grid
 %   xlabel('x')
@@ -20,10 +20,10 @@ function [fun,x,domain] = InterpChebValues(values,x,domain)
 %
 % EXAMPLE2 (Interpolation by Chebyshev polynomial approximation)
 %   n      = 2^5+1;
-%   nodes  = ChebPoints(n,[-pi,pi]);
-%   values = [sin(nodes) cos(nodes) sin(nodes).*cos(nodes)];
-%   x      = linspace(-pi,pi)';
 %   domain = [-pi,pi];
+%   nodes  = ChebPoints(n,domain);
+%   values = [sin(nodes) cos(nodes) sin(nodes).*cos(nodes)];
+%   x      = linspace(domain(1),domain(2))';
 %   fun    = InterpChebValues(values,x,domain);
 %   figure; plot(x,fun,'.-'); grid
 %   xlabel('x')
@@ -32,10 +32,10 @@ function [fun,x,domain] = InterpChebValues(values,x,domain)
 %
 % EXAMPLE3 (CDF/PDF interpolation by Chebyshev polynomial approximation)
 %   n      = 2^7+1;
-%   nodes  = ChebPoints(n,[-8,8]);
+%   domain = [-8,8];
+%   nodes  = ChebPoints(n,domain);
 %   values = [normpdf(nodes) normcdf(nodes)];
 %   x      = linspace(-4,4,101);
-%   domain = [-8, 8];
 %   fun    = InterpChebValues(values,x,domain);
 %   figure; plot(x,fun,'.-'); grid
 %   xlabel('x')
@@ -43,7 +43,7 @@ function [fun,x,domain] = InterpChebValues(values,x,domain)
 %   title('Chebyshev Polynomial Approximation')
 
 % Viktor Witkovsky (witkovsky@gmail.com)
-% Ver.: 27-May-2021 17:56:16
+% Ver.: 28-May-2021 14:28:24
 
 %% FUNCTION
 %  [fun,x,domain] = InterpChebValues(values,x,domain)
@@ -53,40 +53,9 @@ narginchk(1, 3);
 if nargin < 3, domain = []; end
 if nargin < 2, x = []; end
 
-szx = size(x);
-x   = x(:);
-
-% Set x and the domain such that x in [-1,1] and domain = [xMin, xMax]
-if isempty(domain)
-    if isempty(x)
-        domain = [-1,1];
-        x = linspace(-1,1,101)';
-    else
-        domain = [min(x),max(x)];
-        x = 2*(x(:) - domain(1))/ (domain(2)-domain(1)) - 1;
-    end
-else
-    if isempty(x)
-        x = linspace(-1,1,101)';
-    else
-        if (min(x) < domain(1) || max(x) > domain(2))
-            error('Dimension mismatch')
-        else
-            x = 2*(x(:) - domain(1))/ (domain(2)-domain(1)) - 1;
-        end
-    end
-end
-
-coeffs = ChebCoefficients(values);
-n = 0:length(coeffs(:,1))-1;
-
 %% ALGORITHM
 
-fun  = cos(n.*acos(x))*coeffs;
-x    = domain(2)*(x + 1)/2 + domain(1)*(1 - x)/2;
-
-if size(fun,2) == 1
-    fun = reshape(fun,szx);
-end
+coeffs = ChebCoefficients(values);
+[fun,x,domain] = ChebPolyValues(coeffs,x,domain);
 
 end
