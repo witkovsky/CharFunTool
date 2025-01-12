00d8a647b297e3ae021078ca8a6321f369597a44
Viktor <witkovsky@gmail.com>
2025-01-12 14:41:42 +0100

    Create generate_random_numbers_S2.m

---
+++ Applications/PAPER_SupportFiles/PAPER_GofTestCFMedians/MATLAB/generate_random_numbers_S2.m
@@ -0,0 +1,16 @@
+function random_numbers = generate_random_numbers_S2(k,n, N)
+    % Generisanje slučajnih brojeva sa zadatom CDF
+   u = rand(n, N);
+    random_numbers=zeros(n,N);
+    
+    % Primena inverzne transformacije na svaki slučajni broj
+    for i = 1:n
+        for j= 1:N
+        if u(i, j) < 0.5
+            random_numbers(i,j) = (u(i,j) / 2^(k-1))^(1/k);
+        else
+            random_numbers(i, j) = 1 - ((1 - u(i, j)) / 2^(k-1))^(1/k);
+        end
+        end
+    end
+end
